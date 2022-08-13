85a0a5b723cd5f92efc69a085ea57e0b10a06186
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-08-13 20:18:51 +0200

    Set context locally to each file, skip on_page_markdown entirely

--- src/mkdocs_footermatter/plugin.py
+++ src/mkdocs_footermatter/plugin.py
@@ -18,7 +18,6 @@ class FootermatterPlugin(BasePlugin):
     )
 
     def __init__(self):
-        self.data = {}
         self.author_map = {}
 
     def on_config(self, config: config_options.Config):
@@ -36,21 +35,15 @@ class FootermatterPlugin(BasePlugin):
             key, img, url = author.split(self.config.get('separator_map'))
             self.author_map[key.strip()] = [key.strip(), img.strip(), url.strip()]
 
-    def on_page_markdown(self, markdown, page, config, files):
+    def on_page_context(self, context, page, config, nav):
         a = page.meta.get(self.config.get("key_authors"))
         c = page.meta.get(self.config.get("key_created"))
         u = page.meta.get(self.config.get("key_updated"))
         if not a or not c or not u:
-            return markdown
-        locale, now = self.config.get('locale'), datetime.now()
-        self.data['authors'] = a if isinstance(a, list) else [a]
-        self.data['created'] = timeago.format(c, now, locale)
-        self.data['updated'] = timeago.format(u, now, locale)
-
-    def on_page_context(self, context, page, config, nav):
-        if not self.data.get('authors') or not self.data.get('updated') or not self.data.get('created'):
             return context
-        context['footermatter_authors'] = [self.author_map.get(author) for author in self.data.get('authors')]
-        context['footermatter_created'] = self.data.get('created')
-        context['footermatter_updated'] = self.data.get('updated')
+        a = a if isinstance(a, list) else [a]
+        locale, now = self.config.get('locale'), datetime.now()
+        context['footermatter_authors'] = [self.author_map.get(author) for author in a]
+        context['footermatter_created'] = timeago.format(c, now, locale)
+        context['footermatter_updated'] = timeago.format(u, now, locale)
         return context
