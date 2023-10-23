9451f489477dafd103142e1d5c1a7964bd429faf
Sondre Grønås <sondre.gronas@gmail.com>
2023-10-23 16:50:57 +0200

    Remove deprecated pkg_resources, omit 3.7

--- src/mkdocs_footermatter/__init__.py
+++ src/mkdocs_footermatter/__init__.py
@@ -1,9 +1,9 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version, PackageNotFoundError
 
 from mkdocs_footermatter.plugin import FootermatterPlugin
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version('mkdocs-footermatter')
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     pass
